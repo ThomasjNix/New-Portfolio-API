36e08610b128b28c0d4f351f55b2b96e27c77c8d
Thomas Nix <thomasjnix.dev@gmail.com>
2020-12-21 13:19:35 -0800

    Implement single find

--- controllers/experience-controller.js
+++ controllers/experience-controller.js
@@ -13,7 +13,20 @@ const experience_controller = {
         });
     },
     get_one_experience: (req, res, next) => {
-        res.send('TODO');
+        if (req && req.params && req.params.id) {
+            experienceModel.findById(req.params.id).exec((err, result) => {
+                if (err || !result) {
+                    console.log(err);
+                    return res.status(500).send('Database error occured');
+                } else {
+                    result.experienceID = res._id;
+                    res.json(result);
+                }
+            });
+        } else {
+            console.log(err);
+            return res.status(500).send('Failed to load - ID not found');
+        }
     }
 }
 
